a3f1d2e12557266632f10daad04f57653dbbd4bb
shravan <shravan.olgikar13@gmail.com>
2017-07-16 19:35:49 +0530

    added body-parser, modified add view

--- app.js
+++ app.js
@@ -2,10 +2,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 //connection to MongoDb
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/article');
 let db = mongoose.connection;
 
@@ -58,19 +65,31 @@ app.get('/',(req,res) => {
         allArticles:allArticles
       });
     }
-
   });
-
-
-
 });
 
-//Addition routes
+//gets  request routes
 app.get('/addArticle',(req,res)=>{
   res.render('add'  ,{
     someVar:'Add Articles'
   });
 })
+
+//post request routes
+app.post('/addArticle',(req,res)=>{
+  let refArticle = new Article();
+  refArticle.title = req.body.title;
+  refArticle.author = req.body.author;
+  refArticle.body = req.body.body;
+  refArticle.save((err)=>{
+    if (err) {
+      console.log(err);
+    }else {
+      res.redirect('/');
+    }
+  });
+});
+
 
 app.listen(3000,()=>{
   console.log('server start at port 3000');
